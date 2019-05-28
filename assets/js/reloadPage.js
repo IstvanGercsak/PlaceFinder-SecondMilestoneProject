490a6a2d83862afb01533b8058015abb59ffcc86
istvan.gercsak <istvan.gercsak@openjawtech.com>
2019-05-28 14:55:33 +0100

    Styling Reset button. Expand reloadPage function to hide Result button. Delete unused classes.

--- assets/js/reloadPage.js
+++ assets/js/reloadPage.js
@@ -1,5 +1,6 @@
  function reload() {
         $("#map").hide().empty();
+        $("#more").hide();
         $("#places>li").remove();
         document.getElementById("search").value = "";
         window.scrollTo(0, 0);
